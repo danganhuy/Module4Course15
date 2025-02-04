127a1617f7f4f75d08b097ec52269ac75f830bb6
AnHuy <92672519+danganhuy@users.noreply.github.com>
2025-02-04 19:55:39 +0700

    init

---
+++ src/main/java/huy/module4course15/Module4Course15Application.java
@@ -0,0 +1,13 @@
+package huy.module4course15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module4Course15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module4Course15Application.class, args);
+    }
+
+}
---
+++ src/test/java/huy/module4course15/Module4Course15ApplicationTests.java
@@ -0,0 +1,13 @@
+package huy.module4course15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module4Course15ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
